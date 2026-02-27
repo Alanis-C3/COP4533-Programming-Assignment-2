f8e0e03b2eea761030914d3772e7ca2f93816ba0
Alanis-C3 <acastillo3@ufl.edu>
2026-02-27 13:25:02 -0500

    input parsing and example input file

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,48 @@
-//
-// Created by anae2 on 2/27/2026.
-//
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+	vector<string> cacheInput;
+	int cacheCapacity;
+	int m;
+	ifstream inFile;
+	inFile.open("../examplein.txt");
+	if (!inFile.is_open()){
+            cout << "Error opening file" << endl;
+            return -1;
+        }
+	string km;
+	string strM;
+	string strK;
+
+	//getting k & m
+	getline(inFile, km);
+	istringstream iss(km);
+	// separates k and m into sep str
+	iss >> strK >> strM;
+	// convert k and m into int types
+	cacheCapacity = stoi(strK);
+	m = stoi(strM);
+
+	string str;
+	getline(inFile, str);
+	stringstream ss(str);
+	// load request into vector cacheInput
+	for (int i = 0; i < m; i++) {
+		getline(ss, str, ' ');
+		cacheInput.push_back(str);
+	}
+
+	cout << "cache capacity: " << cacheCapacity << endl;
+	cout << "m : " << m << endl;
+	for(int i = 0; i <= m; i++){
+    cout << cacheInput[i] << endl;
+}
+
+
+return 0;}
